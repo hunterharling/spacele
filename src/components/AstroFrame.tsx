d42f684550bf53dd9e0f2ef149c254749ca24a81
HunterHarling <hunterharling@gmail.com>
2022-07-23 13:33:25 -0700

    fix obj fetch

--- src/components/AstroFrame.tsx
+++ src/components/AstroFrame.tsx
@@ -14,40 +14,47 @@ const AstroFrame = () => {
   const api = `https://simbad.u-strasbg.fr/simbad/sim-id?Ident=${obj.split(" ").join("+")}&NbIdent=1&Radius=2&Radius.unit=arcmin&submit=submit+id`;
 
   useEffect(() => {
-    fetch(api)
-      .then(function (response) {
-        return response.text();
-      }).then(function (data) {
-        let coords = data.split('ID="Coord"')[1].split('"')[1];
-        let ra: number;
-        let dec: number;
+    if (!obj.includes("NGC")) {
+      const url = `https://server1.sky-map.org/skywindow?object=${obj.split(" ").join("")}&zoom=${zoom}&img_source=SDSS`;
+      console.log(url)
+      setSrc(url);
+    }
+    else {
+      fetch(api)
+        .then(function (response) {
+          return response.text();
+        }).then(function (data) {
+          let coords = data.split('ID="Coord"')[1].split('"')[1];
+          let ra: number;
+          let dec: number;
 
-        ra = parseInt(
-          coords.split(" ")[0]
-        ) + parseInt(
-          coords.split(" ")[1]) / 60 + parseInt(
-            coords.split(" ")[2]) / 3600;
-        if (coords.includes("+")) {
-          dec = parseInt(
-            coords.split("+")[1].split(" ")[0]
+          ra = parseInt(
+            coords.split(" ")[0]
           ) + parseInt(
-            coords.split("+")[1].split(" ")[1]) / 60 + parseInt(
-              coords.split("+")[1].split(" ")[2]) / 3600;
-        }
-        else {
-          dec = parseInt(
-            coords.split("-")[1].split(" ")[0]
-          ) + parseInt(
-            coords.split("-")[1].split(" ")[1]) / 60 + parseInt(
-              coords.split("-")[1].split(" ")[2]) / 3600;;
-        }
+            coords.split(" ")[1]) / 60 + parseInt(
+              coords.split(" ")[2]) / 3600;
+          if (coords.includes("+")) {
+            dec = parseInt(
+              coords.split("+")[1].split(" ")[0]
+            ) + parseInt(
+              coords.split("+")[1].split(" ")[1]) / 60 + parseInt(
+                coords.split("+")[1].split(" ")[2]) / 3600;
+          }
+          else {
+            dec = parseInt(
+              coords.split("-")[1].split(" ")[0]
+            ) + parseInt(
+              coords.split("-")[1].split(" ")[1]) / 60 + parseInt(
+                coords.split("-")[1].split(" ")[2]) / 3600;;
+          }
 
-        console.log(ra + " " + dec);
+          console.log(ra + " " + dec);
 
-        const url = `https://server1.sky-map.org/skywindow?zoom=${zoom}&img_source=SDSS&ra=${ra}&de=${dec}`;
+          const url = `https://server1.sky-map.org/skywindow?zoom=${zoom}&img_source=SDSS&ra=${ra}&de=${dec}`;
 
-        setSrc(url);
-      });
+          setSrc(url);
+        });
+    }
   }, []);
   return (
     <>
