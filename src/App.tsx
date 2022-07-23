59e29d1db12b8b9db5d30aca1ebcff1145ced067
HunterHarling <hunterharling@gmail.com>
2022-07-23 13:32:52 -0700

    divide components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import './styles/App.css';
 import AstroFrame from './components/AstroFrame';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <div className="header">
-        <h1>ASTRO GUESSER</h1>
-        <div className="btns">
-          <button className="info">
-            <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z" clipRule="evenodd"></path><path d="M5.25 6.033h1.32c0-.781.458-1.384 1.36-1.384.685 0 1.313.343 1.313 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.007.463h1.307v-.355c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.326 0-2.786.647-2.754 2.533zm1.562 5.516c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"></path></svg>
-          </button>
-          <button className="stats">
-            <svg stroke="currentColor" fill="currentColor" strokeWidth="0" version="1.1" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M0 13h16v2h-16zM2 9h2v3h-2zM5 5h2v7h-2zM8 8h2v4h-2zM11 2h2v10h-2z"></path></svg>
-          </button>
-        </div>
-      </div>
+      <Header />
       <h3>Guess which deep sky object this is!</h3>
       <div className="container">
         <AstroFrame />
       </div>
-      <div className="footer"></div>
+      <Footer />
     </div>
   );
 }
