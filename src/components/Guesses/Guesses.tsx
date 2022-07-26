404254587a054ba6b055b8ff2ba412b8909769cf
HunterHarling <hunterharling@gmail.com>
2022-07-26 00:11:08 -0700

    improve mobile styles

--- src/components/Guesses/Guesses.tsx
+++ src/components/Guesses/Guesses.tsx
@@ -1,6 +1,6 @@
 import astrodb from "../../astrodb";
 import Fuse from "fuse.js"
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./Guesses.css";
 import axios from "axios";
 import { Stat } from "../../App";
@@ -20,6 +20,8 @@ const Search = ({ obj, statistics, day }: SearchProps) => {
   const [isOpen, setOpen] = useState(false);
 
   const [tries, setTries] = useState(0);
+
+  const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     // Reset value each day
@@ -79,21 +81,23 @@ const Search = ({ obj, statistics, day }: SearchProps) => {
       setTries(tries + 1);
     }
     setSelections([...selections, choice]);
+    inputRef.current!.value = "";
   }
 
   return (
     <div className="search">
       <div className="selections">
         {selections.map(s =>
-          <div className="selection" key={s}>
+          <div className={"selection"+ (s==obj ? " correct" : "")} key={s}>
             {s}
           </div>)}
       </div>
       <input
+        ref={inputRef}
         type="text"
         placeholder="Pick a deep sky object..."
         onKeyUp={(e) => showOptions(e.target.value)} />
-      {(isOpen && tries < 6) &&
+      {(isOpen && tries < 6 && results.length > 0) &&
         <div className="results">
           {results.map(r =>
             <div
@@ -104,7 +108,7 @@ const Search = ({ obj, statistics, day }: SearchProps) => {
             </div>)}
         </div>}
       {tries > 5 && <h3>This deep sky object is {obj}.</h3>}
-      {isCorrect && <h3>Correct! This deep sky object is {obj}.</h3>}
+      {isCorrect && <h3 className="correct">Correct! This deep sky object is {obj}.</h3>}
     </div>
   );
 }
